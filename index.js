9f4dc94b45afd5e3e146435d3abad972ceaeb594
Sahilji-95 <sahilji9554@gmail.com>
2026-03-09 23:44:42 +0530

    Added home page

--- index.js
+++ index.js
@@ -87,6 +87,10 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.render("listing/home.ejs");
+});
+
 app.all(/(.*)/, (req, res, next) => {
   next(new ExpressError(404, "page not found"));
 });
